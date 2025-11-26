f52d25f6cad0ad200652f4004900799e7c45f849
Bogdan <bodya.kuleba@gmail.com>
2025-11-26 18:52:14 +0200

    LogIn Design Update

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 let usersRegister = [
     {
-        name: 'Vitalik',
-        password: '1234',
+        name: "Vitalik",
+        password: "1234",
     },
 
     {
-        name: 'Bogdan',
-        password: '5555',
+        name: "Bogdan",
+        password: "5555",
     }
 ]
+let logMenuOpened = false
 
-function signTry(inputedUser,inputedPassword) {
+function signTry(inputedUser, inputedPassword) {
     for (let user of usersRegister) {
         if (user === inputedUser) {
-            return true 
+            return true
         } else {
             continue
         }
@@ -23,3 +24,21 @@ function signTry(inputedUser,inputedPassword) {
 }
 
 signTry()
+
+function logInMenu() {
+    if (!logMenuOpened) {
+        $(".emailDiv").css("display", "flex")
+        logMenuOpened = true
+    } else {
+        $(".emailDiv").css("display", "none")
+        logMenuOpened = false
+    }
+}
+
+$("#emailExitBtn").click(() => {
+    logInMenu()
+})
+
+$("#logInEnterBtn").click(() => {
+    logInMenu()
+})
