1a37e50fad66274710a28c930e313deb49454756
Bogdan <bodya.kuleba@gmail.com>
2025-11-29 09:54:08 +0200

    UPDATE SUPER MARIO

--- main.js
+++ main.js
@@ -7,19 +7,81 @@ let usersRegister = [
     {
         name: "Bogdan",
         password: "5555",
-    }
+    },
+
+    {
+        name: "Stepan53",
+        password: "5353",
+    },
+
+    {
+        name: "Sasha",
+        password: "5448",
+    },
 ]
 let logMenuOpened = false
 let notificationOn = false
 
-function signTry(inputedUser, inputedPassword) {
-    for (let user of usersRegister) {
-        if (user.name == inputedUser && user.password == inputedPassword) {
-            return true
-        }
+function messageSystem(message, duration) {
+    if (message && notificationOn == false) {
+        notificationOn = true
+        $(".notiText").text(message)
+        $(".notification").css("top", "9vh")
+        setTimeout(function () {
+            $(".notification").css("top", "-20%")
+            notificationOn = false
+        }, duration)
     }
 }
-signTry()
+
+let lastAcc = localStorage.getItem("lastAccount") || "none"
+
+if (lastAcc !== "none") {
+    $(".notLogInDiv").css("display", "none")
+    $(".logInDivTrue").css("display", "flex")
+    if (lastAcc == "Vitalik") {
+        $(".profilePicture").css("background-image", "url(Images/Vitalik.jpg)")
+        $(".profilePictureHeader").css("background-image", "url(Images/Vitalik.jpg)")
+    } else if (lastAcc == "Bogdan") {
+        $(".profilePicture").css("background-image", "url(Images/Me.png)")
+        $(".profilePictureHeader").css("background-image", "url(Images/Me.png)")
+    } else if (lastAcc == "Stepan") {
+        $(".profilePicture").css("background-image", "url(Images/Stepa.jpg)")
+        $(".profilePictureHeader").css("background-image", "url(Images/Stepa.jpg)")
+    }
+}
+
+function signTry(iUser, iPassword) {
+    let found = false
+
+    for (let user of usersRegister) {
+        if (user.name === iUser && user.password === iPassword) {
+            found = true;
+            $(".notLogInDiv").css("display", "none");
+            $(".logInDivTrue").css("display", "flex");
+
+            if (user.name === "Vitalik") {
+                $(".profilePicture").css("background-image", "url(Images/Vitalik.jpg)");
+                $(".profilePictureHeader").css("background-image", "url(Images/Vitalik.jpg)")
+                localStorage.setItem("lastAccount", "Vitalik")
+            } else if (user.name === "Bogdan") {
+                $(".profilePicture").css("background-image", "url(Images/Me.png)")
+                $(".profilePictureHeader").css("background-image", "url(Images/Me.png)")
+                localStorage.setItem("lastAccount", "Bogdan")
+            } else if (user.name === "Stepan53") {
+                $(".profilePicture").css("background-image", "url(Images/Stepa.jpg)")
+                $(".profilePictureHeader").css("background-image", "url(Images/Stepa.jpg)")
+                localStorage.setItem("lastAccount", "Stepan")
+            }
+
+            break
+        }
+    }
+
+    if (!found) {
+        messageSystem("Login Failed", 2000)
+    }
+}
 
 $("#logInBtn").click(() => {
     let inpUser = $("#usernameInput").val()
@@ -37,22 +99,19 @@ function logInMenu() {
     }
 }
 
-function notification(textContent, duration) {
-    if (notificationOn == false) {
-        notificationOn = true
-        $(".notification").css("top", "-1%")
-        $(".notiText").text(`${textContent}`)
-        setTimeout(function () {
-            $(".notification").css("top", "-20%")
-            notificationOn = false
-        }, 2000)
-    }
-}
-
 $("#emailExitBtn").click(() => {
     logInMenu()
 })
 
 $("#logInEnterBtn").click(() => {
     logInMenu()
+})
+
+$("#logOutBtn").click(() => {
+    messageSystem("Logged out", 2000)
+    $(".logInDivTrue").css("display", "none")
+    $(".notLogInDiv").css("display", "flex")
+    $(".profilePicture").css("background-image", "url(https://cdn-icons-png.flaticon.com/256/3985/3985429.png)")
+    $(".profilePictureHeader").css("background-image", "url(https://cdn-icons-png.flaticon.com/256/3985/3985429.png)")
+    localStorage.setItem("lastAccount", "none")
 })
